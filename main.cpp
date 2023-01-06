69555c134466ca3e4125d59ab08db2d7df3c00e2
ketan Ratan Roy <ketanroy02@gmail.com>
2023-01-06 23:18:46 +0530

    added message in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main()
 {
     int n1;
     int n2;
-    cout << "Enter the first number : " << endl;
+    cout << "Enter the first number: " << endl;
     cin >> n1;
 
-    cout << "Enter the second number : " << endl;
+    cout << "Enter the second number: " << endl;
     cin >> n2;
 
     int sum = n1 + n2;
