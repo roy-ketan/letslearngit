cbf17351706150a92634ca17a73ba2069922b94c
ketan Ratan Roy <ketanroy02@gmail.com>
2023-01-07 00:33:59 +0530

    added implementation for diff.java

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cout << "---------------------------------" << endl;
+    cout << "Enter the first number : " << endl;
+    cin >> a;
+
+    cout << "Enter the second number : " << endl;
+    cin >> b;
+
+    int product = (a * b);
+    cout << "The product is : " << product << endl;
+    return 0;
+}
