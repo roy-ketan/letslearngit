37a1c779697e828728da3eea11b24c5e98492161
ketan Ratan Roy <ketanroy02@gmail.com>
2023-01-06 23:08:09 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1;
+    int n2;
+    cout << "Enter the first number : " << endl;
+    cin >> n1;
+
+    cout << "Enter the second number : " << endl;
+    cin >> n2;
+
+    int sum = n1 + n2;
+
+    cout << "The sum is : " << sum << endl;
+    return 0;
+}
